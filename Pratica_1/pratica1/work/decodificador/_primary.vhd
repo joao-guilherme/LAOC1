c8dabb1b348447ac491cbf31b0f82f880323d7b4
João Guilherme M. Guimarães <joaog95@live.com>
2019-03-29 11:18:16 -0300

    refactor: removido arquivos ignorados, mas ainda rastreados, após a atualização do gitignore

--- Pratica_1/pratica1/work/decodificador/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity decodificador is
-    port(
-        p_Input         : in     vl_logic_vector(1 downto 0);
-        p_Output        : out    vl_logic_vector(7 downto 0)
-    );
-end decodificador;
